46028130f24c76ce7244de30fd35f3bff63a06cd
brandonbljl <113411735+brandonbljl@users.noreply.github.com>
2023-10-31 16:16:36 +0800

    Transmit UART Update
    Transmit 'a' for both UART 0 and 1
    Both UART and SPI TX will lit up for the PICO w board
    only UART TX will be able to transmit the letters
    from there we will be able to determine if the pin is a UART TX or not

--- project/recieveUART/recieveUART.c
+++ project/recieveUART/recieveUART.c
@@ -5,7 +5,7 @@
 // Constants for UART configuration
 #define BAUD_RATE 115200
 
-// Function to receive data on GP1
+// Function to receive data on GP1 for UART0
 void receive_on_gp1() {
     uart_init(uart0, BAUD_RATE);
     gpio_set_function(1, GPIO_FUNC_UART);  // Configure GP1 for UART
@@ -20,12 +20,30 @@ void receive_on_gp1() {
     }
 }
 
+// Function to receive data on GP5 for UART1
+void receive_on_gp5() {
+    uart_init(uart1, BAUD_RATE);
+    gpio_set_function(5, GPIO_FUNC_UART);  // Configure GP5 for UART
+
+    while (true) {
+        // Check for received characters
+        while (uart_is_readable(uart1)) {
+            char receivedChar = uart_getc(uart1);
+            printf("Received data on GP5 (UART1 RX pin 5): %c\n", receivedChar);
+            // Add your specific handling code for GP5 here
+        }
+    }
+}
+
 int main() {
     stdio_init_all();
 
-    printf("Receiving data on GP1 (UART RX pin 1):\n");
-
+    printf("Receiving data on GP1 (UART0 RX pin 1):\n");
     receive_on_gp1();
+
+    // Add a message for receiving on GP5 (UART1 RX pin 5)
+    printf("Receiving data on GP5 (UART1 RX pin 5):\n");
+    receive_on_gp5();
 
     return 0;
 }
--- project/transmitUART/transmitUART.c
+++ project/transmitUART/transmitUART.c
@@ -6,51 +6,64 @@
 // Constants for UART configuration
 #define BAUD_RATE 115200
 
-// Function to transmit 'a' from a specific GP pin
-void transmit_a_from_pin(int gpio_pin) {
+// Function to transmit 'a' from a specific GP pin for UART0
+void transmit_a_from_pin_uart0(int gpio_pin) {
     // Initialize UART communication with the specified baud rate for UART0
     uart_init(uart0, BAUD_RATE);
 
-    // Configure the GPIO pin for UART transmission (make sure to select the correct UART module)
+    // Configure the GPIO pin for UART0 transmission
     gpio_set_function(gpio_pin, GPIO_FUNC_UART);
     uart_set_baudrate(uart0, BAUD_RATE);
 
-    // Check if UART TX is writable
+    // Check if UART0 TX is writable
     if (uart_is_writable(uart0)) {
-        // Transmit 'a' from the current GPIO pin
+        // Transmit 'a' from the current GPIO pin using UART0
         uart_putc(uart0, 'a');
     } else {
-        printf("GP%d is not UART TX - Not transmitting.\n", gpio_pin);
+        printf("GP%d is not UART0 TX - Not transmitting.\n", gpio_pin);
+    }
+}
+
+// Function to transmit 'a' from a specific GP pin for UART1
+void transmit_a_from_pin_uart1(int gpio_pin) {
+    // Initialize UART communication with the specified baud rate for UART1
+    uart_init(uart1, BAUD_RATE);
+
+    // Configure the GPIO pin for UART1 transmission
+    gpio_set_function(gpio_pin, GPIO_FUNC_UART);
+    uart_set_baudrate(uart1, BAUD_RATE);
+
+    // Check if UART1 TX is writable
+    if (uart_is_writable(uart1)) {
+        // Transmit 'a' from the current GPIO pin using UART1
+        uart_putc(uart1, 'a');
+    } else {
+        printf("GP%d is not UART1 TX - Not transmitting.\n", gpio_pin);
     }
 }
 
 int main() {
     stdio_init_all();
 
-    printf("Transmitting 'a' from specified GP pins simultaneously:\n");
+    printf("Transmitting 'a' from specified GP pins to UART0 and UART1 simultaneously:\n");
 
     // Define an array of GP pins to transmit from
     int tx_pins[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 26, 27, 28};
     int num_pins = sizeof(tx_pins) / sizeof(tx_pins[0]);
 
-    // Initialize UART communication with the specified baud rate for UART0 once
+    // Initialize UART communication with the specified baud rate for UART0 and UART1 once
     uart_init(uart0, BAUD_RATE);
+    uart_init(uart1, BAUD_RATE);
 
     while (1) {
         for (int i = 0; i < num_pins; i++) {
             int gpio_pin = tx_pins[i];
-            printf("Transmitting 'a' from GP%d\n", gpio_pin);
-            transmit_a_from_pin(gpio_pin);
+            printf("Transmitting 'a' from GP%d to UART0\n", gpio_pin);
+            transmit_a_from_pin_uart0(gpio_pin);
+            printf("Transmitting 'a' from GP%d to UART1\n", gpio_pin);
+            transmit_a_from_pin_uart1(gpio_pin);
         }
         sleep_ms(1000); // Wait for 1 second before transmitting again
-        
-        // Check the state of GPIO pins and print "Not UART TX" if not transmitting
-        for (int i = 0; i < num_pins; i++) {
-            int gpio_pin = tx_pins[i];
-            if (gpio_get(gpio_pin) == 0) {
-                printf("GP%d is not UART TX - Not transmitting.\n", gpio_pin);
-            }
-        }
     }
 
     return 0;
